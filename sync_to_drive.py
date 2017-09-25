5180f5625acd3ec726a5527bf55ff9698a496608
Bruno Duyé <brunetton@gmail.com>
2017-09-26 00:06:16 +0200

    WIP: notebooks

--- sync_to_drive.py
+++ sync_to_drive.py
@@ -17,11 +17,14 @@ from docopt import docopt
 from sync_utils import *
 
 
+NOTEBOOKS_PATH = os.path.expanduser("~/dev/dbnomics/notebooks/")
+
 args = docopt(__doc__.format(self_filename=os.path.basename(__file__)))
 dest_path = args['<usb_key_path>']
 
 assert os.path.exists(os.path.expanduser("~/zim-notes"))
 assert os.path.exists(dest_path) and os.path.isdir(dest_path)
+assert os.path.exists(NOTEBOOKS_PATH) and os.path.isdir(NOTEBOOKS_PATH)
 # Check if backup file exists
 today_backup_filename = "zim-notes_{}.tgz".format(datetime.datetime.now().strftime('%Y%m%d'))
 today_backup_filepath = os.path.join(os.path.expanduser("~/"), today_backup_filename)
@@ -40,4 +43,11 @@ assert not os.path.exists(today_usb_backup_filepath), "{} already exists".format
 print "=> backup to {}".format(today_usb_backup_filepath)
 shutil.copy2(today_backup_filepath, today_usb_backup_filepath)
 assert os.path.exists(today_usb_backup_filepath) and os.path.getsize(today_usb_backup_filepath) > 0
+
+# Notebooks
+noteboooks_dest_path = os.path.join(dest_path, 'notebooks')
+print "=> Copying notebooks to {}".format(dest_path)
+assert not os.path.exists(noteboooks_dest_path)
+shutil.copytree(NOTEBOOKS_PATH, noteboooks_dest_path)
+
 print("=> end")
