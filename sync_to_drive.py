1b7c19f97ced6f8a3059fa5fd1cb7dfe7917e95f
Bruno Duyé <brunetton@gmail.com>
2017-09-26 01:28:16 +0200

    add backup() function

--- sync_to_drive.py
+++ sync_to_drive.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Sync from computer => usb key
@@ -18,6 +18,7 @@ from sync_utils import *
 
 
 NOTEBOOKS_PATH = os.path.expanduser("~/dev/dbnomics/notebooks/")
+ZIM_PATH = os.path.expanduser("~/zim-notes/")
 
 args = docopt(__doc__.format(self_filename=os.path.basename(__file__)))
 dest_path = args['<usb_key_path>']
@@ -25,29 +26,37 @@ dest_path = args['<usb_key_path>']
 assert os.path.exists(os.path.expanduser("~/zim-notes"))
 assert os.path.exists(dest_path) and os.path.isdir(dest_path)
 assert os.path.exists(NOTEBOOKS_PATH) and os.path.isdir(NOTEBOOKS_PATH)
-# Check if backup file exists
-today_backup_filename = "zim-notes_{}.tgz".format(datetime.datetime.now().strftime('%Y%m%d'))
-today_backup_filepath = os.path.join(os.path.expanduser("~/"), today_backup_filename)
-if os.path.exists(today_backup_filepath):
-    # add timestamp to filename
-    today_backup_filename = "zim-notes_{}.tgz".format(datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
-    today_backup_filepath = os.path.join(os.path.expanduser("~/"), today_backup_filename)
-    assert not os.path.exists(today_backup_filepath)
-# Backup to drive
-print "=> backup to {}".format(today_backup_filepath)
-shell("tar cf {}  -C {} zim-notes".format(today_backup_filepath, os.path.expanduser("~/")))
-assert os.path.exists(today_backup_filepath) and os.path.getsize(today_backup_filepath) > 0
-# Backup to usb drive
-today_usb_backup_filepath = os.path.join(dest_path, today_backup_filename)
-assert not os.path.exists(today_usb_backup_filepath), "{} already exists".format(today_usb_backup_filepath)
-print "=> backup to {}".format(today_usb_backup_filepath)
-shutil.copy2(today_backup_filepath, today_usb_backup_filepath)
-assert os.path.exists(today_usb_backup_filepath) and os.path.getsize(today_usb_backup_filepath) > 0
 
+def backup(source_file_or_dir, local_backup_path, dest_backup_path):
+    """ - make file-timestamp.tgz on local_backup_path
+        - copy this backup to dest_backup_path
+    All paths can be with "~"
+    """
+    assert os.path.exists(source_file_or_dir)
+    assert os.path.exists(dest_backup_path) and os.path.isdir(dest_backup_path)
+    print("=> Backup {}".format(source_file_or_dir))
+    # expanduser
+    source_file_or_dir = os.path.expanduser(source_file_or_dir)
+    local_backup_path = os.path.expanduser(local_backup_path)
+    dest_backup_path = os.path.expanduser(dest_backup_path)
+    # Check if backup file exists
+    source_filename = os.path.basename(os.path.normpath(source_file_or_dir))
+    today_backup_filename = "{}_{}.7z".format(source_filename, datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
+    today_backup_filepath = os.path.join(local_backup_path, today_backup_filename)
+    assert not os.path.exists(today_backup_filepath)
+    # Backup to dest
+    shell("7z a {} {}".format(today_backup_filepath, source_file_or_dir), capture_output=True)
+    assert os.path.exists(today_backup_filepath) and os.path.getsize(today_backup_filepath) > 0
+    # Backup to dest
+    dest_backup_filepath = os.path.join(dest_path, today_backup_filename)
+    assert not os.path.exists(dest_backup_filepath), "{} already exists".format(dest_backup_filepath)
+    print("  - backup to {}".format(dest_backup_filepath))
+    shutil.copy2(today_backup_filepath, dest_backup_filepath)
+    assert os.path.exists(dest_backup_filepath) and os.path.getsize(dest_backup_filepath) > 0
+
+# Zim
+backup(ZIM_PATH, "~", dest_path)
 # Notebooks
-noteboooks_dest_path = os.path.join(dest_path, 'notebooks')
-print "=> Copying notebooks to {}".format(dest_path)
-assert not os.path.exists(noteboooks_dest_path)
-shutil.copytree(NOTEBOOKS_PATH, noteboooks_dest_path)
+backup(NOTEBOOKS_PATH, "~", dest_path)
 
 print("=> end")
